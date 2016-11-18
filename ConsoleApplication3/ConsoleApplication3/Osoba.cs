6ee2a2685f1bb8d3975fe5ec376ec9bc5210a70a
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-18 17:47:16 +0100

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Osoba
+    {
+        private int wiek; //deklaracja pola
+        public Osoba() { } //konstruktor domyślny
+        public Osoba(int wiek) //konstruktor parametryczny
+        {
+            this.wiek = wiek;
+        }
+        public void UstawWiek(int wiek) //metoda na ustawienie wieku
+        {
+            this.wiek = wiek;
+        }
+        public int PobierzWiek() //metoda na pobranie wieku
+        {
+            return wiek;
+        }
+    }
+}
